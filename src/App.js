34d7411f95aa921e148c3e1d7cd786dd0fd80f2d
Lim Yee Han <limyeehan@gmail.com>
2020-06-16 02:06:25 +0800

    React-router-dom starter pack

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import { ThemeProvider } from "@material-ui/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import { Switch, Route } from "react-router-dom";
+
+import Nav from "./components/Nav";
+import AboutMe from "./components/AboutMe";
+import Contact from "./components/Contact";
+import LandingPage from "./components/LandingPage";
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
+
+export default function App() {
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#00de00",
+      },
+      secondary: {
+        main: "#ee4b4b",
+      },
+      background: {
+        default: "#a0a0a5",
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Nav />
+        <Switch>
+          <Route path="/" component={LandingPage} />
+          <Route path="/aboutme" component={AboutMe} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/landingpage" component={LandingPage} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/resume" component={Resume} />
+        </Switch>
+      </ThemeProvider>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/AboutMe.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AboutMe() {
+  return (
+    <div>
+      <h1>AboutMe</h1>
+    </div>
+  );
+}
+
+export default AboutMe;
