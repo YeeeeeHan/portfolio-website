fa053c98c3dc35a19093e6e35f7affb45b8c80a9
Lim Yee Han <limyeehan@gmail.com>
2020-06-17 00:10:10 +0800

    Added new_feature2

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import LandingPage from "./components/LandingPage";
 import Projects from "./components/Projects";
 import Resume from "./components/Resume";
 
-export default function App() { // new feature
+export default function App() { // new feature2
   const [darkMode, setDarkMode] = useState(false);
   const theme = createMuiTheme({
     root: {
